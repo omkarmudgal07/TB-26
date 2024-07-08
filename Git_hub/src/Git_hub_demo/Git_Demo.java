0b27a2a4296e3bf9b9b4db156ce20c24060ff950
omkarmudgal07 <mudgalomkar3@gmail.com>
2024-07-08 10:08:56 +0530

    Update Git_Demo.java
    by omkar

--- Git_hub/src/Git_hub_demo/Git_Demo.java
+++ Git_hub/src/Git_hub_demo/Git_Demo.java
@@ -2,6 +2,14 @@ package Git_hub_demo;
 
 public class Git_Demo {
 
+	public void m1()
+	{
+  system.out.println("this is m1")
+
+		
+	}
+	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
